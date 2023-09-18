3baa199238aceb39988c6000d7fad84727894ca6
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-09-17 20:13:15 -0700

    Update study.js

--- study.js
+++ study.js
@@ -2,3 +2,17 @@
 
 //to print output
 console.log("Hello");
+
+//variables 
+let name = "Ivan";
+var address = "New York";
+/*The difference between let and var 
+is in the scope of the variables they create: 
+Variables declared by let are only available 
+inside the block where they're defined. 
+Variables declared by var are available 
+throughout the function in which they're declared*/
+const yearOfBirth = 1989;
+/*This means that the variable can be used just as 
+any other variable of its type, but its value cannot 
+be changed.*/
