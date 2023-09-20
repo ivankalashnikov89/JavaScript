70f51e5dad39b86a0b19c1e4680081c973362104
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-09-19 20:01:25 -0700

    Update study.js

--- study.js
+++ study.js
@@ -22,7 +22,7 @@ be changed.*/
 
 //---------------FUNCTIONS----------------
 
-
+//Function declaration
 // EX1 Defining a function that returns a sum
 function add(a, b) {
     return a + b;
@@ -32,8 +32,6 @@ function add(a, b) {
   const result = add(3, 4);
   console.log(result); // Output: 7
 
-
-
   // EX2 Defining a function
 function greet(name) {
     console.log(`Hello, ${name}!`);
@@ -42,3 +40,49 @@ function greet(name) {
   // Calling the function
   greet("John"); // Output: Hello, John!
   greet("Alice"); // Output: Hello, Alice!
+
+
+
+  // Function expression
+const greet = function(name) {
+    console.log(`Hello, ${name}!`);
+  };
+  
+  // Calling the function
+  greet("John"); // Output: Hello, John!
+  greet("Alice"); // Output: Hello, Alice!
+
+
+
+  /*Arrow functions are a concise way to write 
+  functions in JavaScript, introduced in 
+  ECMAScript 6 (ES6). They offer a more compact 
+  syntax compared to traditional function expressions, 
+  making them particularly useful for short, simple 
+  functions. Here's how you define an arrow function: */
+
+  // Traditional function expression
+const add = function(a, b) {
+    return a + b;
+  };
+  
+  // Arrow function
+  const addArrow = (a, b) => a + b;
+
+  // Arrow function with no parameters
+const sayHello = () => console.log("Hello!");
+
+// Arrow function with one parameter
+const square = x => x * x;
+
+// Arrow function with multiple statements
+const greet = name => {
+  const greeting = `Hello, ${name}!`;
+  console.log(greeting);
+};
+
+sayHello(); // Output: Hello!
+console.log(square(5)); // Output: 25
+greet("Alice"); // Output: Hello, Alice!
+
+  
